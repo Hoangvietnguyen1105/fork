10c92bd704e3eebb2847db06cfa4844fb2dceec5
hoang viet <hoangvietnguyen1105@gmail.com>
2023-06-02 11:10:41 +0700

    helo

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+import { Application, Sprite } from 'pixi.js'
+
+const app = new Application({
+    resolution: window.devicePixelRatio || 1,
+    backgroundColor: 2,
+    width: 720,
+    height: 1280
+});
+console.log("abc")
+document.body.appendChild(app.view);
+var clampy = Sprite.from("test.png");
+clampy.anchor.set(0.5);
+clampy.x = app.screen.width / 2;
+clampy.y = app.screen.height / 2;
+app.stage.addChild(clampy);
